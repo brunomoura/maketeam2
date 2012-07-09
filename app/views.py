b7c6cf258db989506c2863cf809188eb46c8ba3d
Bruno Moura <brunolmoura@gmail.com>
2012-07-09 19:12:54 -0300

    marcacao

--- app/views.py
+++ app/views.py
@@ -26,10 +26,9 @@ def compartilhar(request):
 
 	dados = request.POST
 	token = request.POST['token']
-	# user = dados["dados[8][id]"]
 	imagem = montar_imagem(dados)
-	
+	# tags = montar_marcacao(dados)
 	signed_request = SignedRequest.parse(token, settings.FACEBOOK_APPLICATION_SECRET_KEY)
 	graph = GraphAPI(signed_request['oauth_token'])
-	post = graph.post(path="me/photos", tags="[{'tag_uid':'100003016125914', 'x':'30', 'y':'30'},{'tag_uid':'100003798651396'}]", message="Vote na sua banda favorita e acompanhem em tempo real essa disputa!!! acesse: http://apps.facebook.com/maketeam2", source=open(imagem))
+	post = graph.post(path="me/photos", tags="[{'tag_uid':'100003016125914'},{'tag_uid':'100003798651396'}]", message="Vote na sua banda favorita e acompanhem em tempo real essa disputa!!! acesse: http://apps.facebook.com/maketeam2", source=open(imagem))
 	return HttpResponse(json.dumps(post))
